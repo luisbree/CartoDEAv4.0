ace42a94269d23901c2ee74062e3b2c04c99bec5
Luis Bree <LuisBree@gmail.com>
2025-12-11 15:27:23 +0000

    Try fixing this error: `Runtime Error: Error: Cannot create property 'cu

--- src/components/panels/AnalysisPanel.tsx
+++ src/components/panels/AnalysisPanel.tsx
@@ -392,7 +392,7 @@ const AnalysisPanel: React.FC<AnalysisPanelProps> = ({
     // State for clicked profile points
     const [profilePoints, setProfilePoints] = useState<Feature<Point>[]>([]);
     const profilePointsLayerRef = useRef<VectorLayer<VectorSource<Point>> | null>(null);
-    const profilePointsSourceRef = useRef<VectorSource<Point>> | null>(null);
+    const profilePointsSourceRef = useRef<VectorSource<Point> | null>(null);
     const [averageVectorLayerId, setAverageVectorLayerId] = useState<string | null>(null);
 
 
